7c5055b346b9e7aec9bbf49588a920d39a3027a9
Alex-0s <alexostle87@gmail.com>
2024-10-07 15:34:54 +0200

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -1,15 +1,48 @@
+import java.util.ArrayList;
+
+import lejos.robotics.SampleProvider;
 
 public class Robot {
 
-	Sensors sensors = new Sensors();
-	Actionneurs actionneurs = new Actionneurs();
+	private Sensors sensors;
+	private Actionneurs actionneurs;
 	
+    public Robot() {
+        sensors = new Sensors();
+        actionneurs = new Actionneurs();
+    }
+        
 	public void Ramasser() {
 		actionneurs.ouvrirPinces(10, 30);
 		while(!sensors.getTouch()) actionneurs.avancer(10);
 		if(sensors.getTouch()) actionneurs.fermerPinces(10,30);
 	}
-	 
-	 
-
+	
+	public void Rechercher() {
+		SampleProvider distanceProvider = sensors.getDistance();
+		ArrayList distances = new ArrayList();
+		
+			float[] sample = new float[distanceProvider.sampleSize()];
+	        distanceProvider.fetchSample(sample, 0);
+	        distances.add(Float.valueOf(sample[0]));	
+		
+	}
+	
+	public void DeplacerVersPalet() {
+		
+	}
+	
+	public void Deposer() {
+		actionneurs.ouvrirPinces(10, 30);
+	}
+	
+	public void PremierPalet() {
+		actionneurs.arc(10, 10);
+		Ramasser();
+		actionneurs.avancer(10);
+		actionneurs.ouvrirPinces(10, 10);
+	}
+	
+   
 }
+
