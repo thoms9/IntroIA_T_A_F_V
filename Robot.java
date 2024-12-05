7c03d6626fc6f88c41429ed9036591c6ad26d5ff
Alex-0s <alexostle87@gmail.com>
2024-12-05 09:42:00 +0100

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -1,8 +1,5 @@
 import java.util.ArrayList;
 import java.util.List;
-
-import lejos.hardware.motor.*;
-import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
 
 public class Robot {
@@ -10,296 +7,362 @@ public class Robot {
 	private Sensors sensors;
 	private Actionneurs actionneurs;
 	private List<Float> distances;
-	private int orientation;
+	private double orientation;
+	private int indicePalet;
+	private Float distancePalet;
+	private int premiereCouleur;
 	
     public Robot() {
         sensors = new Sensors();
         actionneurs = new Actionneurs();
         distances = new ArrayList<>();
         orientation = 0;
+        indicePalet = -1;
+        distancePalet = 3f;
+        premiereCouleur = -1;
     }
-        
-	public void ramasser() {
-		actionneurs.setLinearSpeed(120);
-		actionneurs.ouvrirPinces(1000);
-		while(!sensors.getTouch()) { 
-			actionneurs.avancer(100, true);
-			//Delay.msDelay(500);
-		}
-		if(sensors.getTouch()) actionneurs.fermerPinces(1000);
-	}
-	
-public void rechercher() {
+    
+    public void rechercher() {
 		
-		actionneurs.setRotationSpeed(50);
-		
+		actionneurs.setRotationSpeed(150);
 	    distances.clear();
-	    
 	    actionneurs.tourner(360.0, true);
 	    
 	    float[] sample = new float[sensors.getDistance().sampleSize()];
 	    
 	    while (actionneurs.isMoving()) {
-	        sensors.getDistance().fetchSample(sample, 0);
-	        
+	        sensors.getDistance().fetchSample(sample, 0);   
 	        distances.add(sample[0]);
-	        
-	        Delay.msDelay(100);
-	    }
-	    
-	    sensors.closeSensors();
-	    
-	    System.out.println("Distances : " + distances);
-	    System.out.println(distances.size());
-	    
+	        Delay.msDelay(15);
+	    }	
 	}
 
-	
-        /*
-	 * Retourne un boolean qui détermine si la liste des distances est vide,
-	 * vrai si la liste est vide, faux si la liste n'est pas vide. 
-	 */
-	public boolean distanceVide() {
+	public List<Integer> indicesDistances() {
+		List<Integer> indices = new ArrayList<>();	
+		double angle = (double)360/distances.size();
+		for (int i = 0; i < distances.size(); i++) {	
+	    	if(i<(90/angle))
+	    			indices.add(-2);
+	    	else if(i>(270/angle))
+	    			indices.add(-2);
+	    	else if(distances.get(i)>2.5)
+	    		indices.add(-2);
+	    	else indices.add(i);
+		 }
+	    return indices;
+	 }
+    
+    public List<Integer> indicesDebutFin(List<Integer> indices) {
+    	List<Integer> indicesDebutFin = new ArrayList<>();
+        final double tolerance = 0.3;
+            for (int i = 1; i < distances.size(); i++) {
+                double difference = Math.abs(distances.get(i) - distances.get(i - 1));
+                if(indices.get(i)==-2) {
+                	continue;
+                }
+                else if (difference > tolerance) {
+                	indicesDebutFin.add(indices.get(i));
+                }
+            }
+
+            return indicesDebutFin;
+    }
+    
+    public List<Integer> indicesPalets(List<Integer> indicesDebutFin) {
+	    List<Integer> indicesPalets = new ArrayList<>();
+	    for (int i=0; i<indicesDebutFin.size()-1; i+=2) {
+	    	int debut = indicesDebutFin.get(i);
+	    	int fin = indicesDebutFin.get(i+1);
+	   		int moy = (debut+fin)/2;
+	   		indicesPalets.add(moy);
+	   	}
+	   	return indicesPalets;
+    }
+    
+    public Float distancePalet(List<Integer> indicesPalets) {
+    	for (int i=0; i<indicesPalets.size();i++) {
+    		if(distances.get(indicesPalets.get(i))<distancePalet) {
+	    		distancePalet = distances.get(indicesPalets.get(i));
+	   		}
+	   	}
+	   	return distancePalet;
+     }
+    
+    public int orientationMin(List<Integer> indicesPalets) {
+    	float distanceMin = 3;
+    	int indice = -1;
+	   	for (int i=0; i<indicesPalets.size();i++) {
+	   		if(distances.get(indicesPalets.get(i))<distanceMin) {
+	   			distanceMin = distances.get(indicesPalets.get(i));
+	   			indice = indicesPalets.get(i);
+    		}
+	   	}
+	   	return indice;
+	 }
+    
+    public void trouverPalet() {
+    	
+    	rechercher();
 		
-	    if (distances.isEmpty()) {
-	        System.out.println("Aucune donnée de distance disponible.");
-	        return true;
-	    }
-	    
-	    return false;
+		List<Integer> indices = indicesDistances();
+        List<Integer> indicesDebutFin = indicesDebutFin(indices);
+        List<Integer> indicesPalets = indicesPalets(indicesDebutFin);
+        distancePalet = distancePalet(indicesPalets);
+        indicePalet = orientationMin(indicesPalets);
+        System.out.println("indices Palets: " + indicesPalets);
+        System.out.println("distance palet le plus proche: " +distancePalet + " m");
+        System.out.println("orientation palet le plus proche: "
+        		+ indicePalet*(double)360/distances.size() + " degrès");
+        if(distances.isEmpty() || indicesDebutFin.isEmpty() || indicesPalets.isEmpty() ||
+        		distancePalet==3 || indicePalet==-1) {
+        	actionneurs.reculer(30, false);
+        	trouverPalet();
+        }
+        	
 	}
 	
-	
-	
-	/*
-	 * Retourne une HashMap contenant les indices (entiers) et les distances (float) en mètres,
-	 */
-	public Map<Integer, Float> indicesPalets() {
+	public void deplacerVersPalet() {
 		
-		Map<Integer, Float> distanceMap = new HashMap<>();
+		actionneurs.setRotationSpeed(75);
+		actionneurs.setLinearSpeed(100);
+		actionneurs.tourner(orientation, false);
+		actionneurs.avancer(distancePalet*100+25, true);
 		
-	    for (int i = 0; i < distances.size(); i++) {				
-	        distanceMap.put(i, distances.get(i));
-	    }
-	    
-	    return distanceMap;
+		while(actionneurs.isMoving()) {
+			Delay.msDelay(20);
+			float distance = distanceDevant();
+			if(distance < distancePalet) {
+				distancePalet = distance;
+			}
+			else {
+				actionneurs.arreter();
+				ramasser();
+			}
+		}       
 	}
 	
-	
-	
-	/*
-	 * Retourne une liste d'entier des indices des palets filtrés
-	 * Prend comme paramètre une HashMap contenant les indices et les distances.
-	 */
-	public List<Integer> indicesFiltres(Map<Integer, Float> map) {
-		
-		List<Integer> indicesFiltres = new ArrayList<>();
-
-	    for (int i = 0; i < distances.size(); i++) {
-	        Float value = map.get(i);
-	        if (value != null && value > 0.3  && value <2) indicesFiltres.add(i);
-	    }
-	    
-	    if (indicesFiltres.isEmpty()) {
-	        System.out.println("Aucun indice filtre.");
-	    }
-	    
-	    return indicesFiltres;
+	public Float distanceDevant() {
+		float[] sample = new float[sensors.getDistance().sampleSize()];
+		sensors.getDistance().fetchSample(sample, 0);
+		return sample[0];
 	}
 	
-	
-	
-	/*
-	 * Retourne une liste d'entier des indices des palets potentiels visibles. 
-	 * Prend en premier paramètres une HashMap contenant les indices et les distances,
-	 * et en deuxième paramètre une liste des indices filtrés.
-	 */
-	public List<Integer> indicesPalets(Map<Integer, Float> map, List<Integer> indices) {
+	public void ramasser() {
+        actionneurs.setLinearSpeed(50);
+        actionneurs.ouvrirPinces(750, true);
+        actionneurs.avancer(45, true);
+        
+        while(actionneurs.isMoving()) {
+        	 if (sensors.getTouch()) {
+        		 actionneurs.fermerPinces(750,false);
+             	 actionneurs.arreter();
+             }
+        	
+        }
+    }
+
+	public void allerZoneDepot() {
+		switch (premiereCouleur) {
+			
+		case lejos.robotics.Color.BLUE:
+			allerZoneDepotVert();
 		
-		float tolerance = 0.21f;
-		List<Integer> indicesPalets = new ArrayList<>();
-	    
-	    for (int i = 0; i < indices.size(); i++) {
-	    	Float courant = (i >= 0 && i < indices.size()) ? map.get(indices.get(i)) : null;
-	        Float prochain = (i + 1 < indices.size()) ? map.get(indices.get(i+1)) : null;
-	        Float precedent = (i - 1 >= 0) ? map.get(indices.get(i-1)) : null;
-	        
-	        if(courant==null) continue;	        	
-	        
-	        if ((precedent != null && Math.abs(courant - precedent) <= tolerance) ||
-	                (prochain != null && Math.abs(courant - prochain) <= tolerance)) {
-	                indicesPalets.add(indices.get(i));
-	            }
-	        
-	        if ((precedent != null && Math.abs(courant - precedent) <= tolerance) &&
-	                !(prochain != null && Math.abs(courant - prochain) <= tolerance)) {
-	                indicesPalets.add(0);
-	            }	        	
-	    }
-	    
-	    if (indicesPalets.isEmpty()) {
-	    	System.out.println("Aucun palets detecte.");
-	    }
-	    
-	    return indicesPalets;
+		case lejos.robotics.Color.GREEN:
+			allerZoneDepotBlue();
+			
+		default:
+			break;
+		}
 	}
 	
-	
-	
-	/*
-	 * Retourne une liste composée de listes des indices (entiers) de chaque palet.
-	 * Prend comme paramètre une liste des palets potentiels.
-	 */
-	public ArrayList<ArrayList<Integer>> palets(List<Integer> indices) {
+    public void allerZoneDepotVert() {
 		
-	    ArrayList<ArrayList<Integer>> palets = new ArrayList<>();
-	    ArrayList<Integer> palet = new ArrayList<>();
-
-	    for (int i = 0; i < indices.size(); i++) {
-	        int courant = indices.get(i);
-	        int precedent = (i > 0) ? indices.get(i - 1) : -1;
-
-	        if (courant == precedent + 1) {
-	            palet.add(courant);
-	        } 
-	        
-	        else {
-	            if (palet.size() > 0) {
-	                if (!(palet.size() == 1 && palet.get(0) == 0)) {
-	                    if (palet.size() <= 3) {
-	                        palets.add(new ArrayList<>(palet));
-	                    }
-	                }
-	                palet.clear();
-	            }
-	            palet.add(courant);
-	        }
-	    }
-
-	    if (palet.size() > 0 && palet.size() <= 3 && !(palet.size() == 1 && palet.get(0) == 0)) {
-	        palets.add(new ArrayList<>(palet));
-	        palet.clear();
-	    }
-
-	    return palets;
+		actionneurs.avancer(10, false);
+				
+		if(orientation < 180) {
+			actionneurs.tourner(90-orientation, false);
+		}
+		
+		else {
+			actionneurs.tourner(90+orientation, false);
+		}
+		
+		actionneurs.setLinearSpeed(75);
+		actionneurs.avancer(Double.MAX_VALUE, true);
+		
+		while(actionneurs.isMoving()) {
+			Delay.msDelay(20);
+			float distance = distanceDevant();
+			if(distance > 0.15) {
+				continue;
+			}
+			else {
+				actionneurs.arreter();
+				if(orientation < 180) {
+					actionneurs.tourner(-90, false);
+				}
+				
+				else actionneurs.tourner(90, false);
+				break;
+			}
+		}
 	}
 
-	
-	
-	/*
-	 * Retourne un float contenant la distance en mètre du palet le plus proche du robot.
-	 * Prend comme premier paramètre une liste composée de listes des indices (entiers) de chaque palets,
-	 * et comme deuxième paramètre une HashMap contenant les indices (entiers) et les distances (float) en mètres.
-	 */
-	public float paletLePlusProche(ArrayList<ArrayList<Integer>> palets, Map<Integer, Float> map) {
+	public void allerZoneDepotBlue() {
 		
-		ArrayList<Integer> palet = new ArrayList<>();
-		float distanceMin = 3;
-	    
-	    for(int i=0; i<palets.size();i++) {
-	    	palet = palets.get(i);
-	    	
-	    	if(palet.size()==0) {
-	    		continue;
-	    	}
-	    	
-	    	float sum = 0;
-	    	int indiceSum = 0;
-	        for (int j=0; j< palet.size(); j++) {
-	              sum += map.get(palet.get(j));
-	              indiceSum+=palet.get(j);
-	        }
-	        
-	        if(sum/palet.size()<distanceMin) {
-	        	distanceMin=sum/palet.size();
-	        }
-	    }
-	    
-	    return distanceMin;
+		actionneurs.avancer(10, false);
+				
+		if(orientation < 180) {
+			actionneurs.tourner(90+orientation, false);
+		}
+		
+		else {
+			actionneurs.tourner(90-orientation, false);
+		}
+		
+		actionneurs.setLinearSpeed(75);
+		actionneurs.avancer(Double.MAX_VALUE, true);
+		
+		while(actionneurs.isMoving()) {
+			Delay.msDelay(20);
+			float distance = distanceDevant();
+			if(distance > 0.15) {
+				continue;
+			}
+			else {
+				actionneurs.arreter();
+				if(orientation < 180) {
+					actionneurs.tourner(90, false);
+				}
+				
+				else actionneurs.tourner(-90, false);
+				break;
+			}
+		}
 	}
-
-	
-	
-	/*
-	 * Méthode qui détermine le palets le plus proche du robot et son orientation.
-	 */
-	public void trouverPalet() {
-		
-		if (distanceVide());
-		
-	    Map<Integer, Float> distanceMap = indicesPalets();
-	    
-	    List<Integer> indicesFiltres = indicesFiltres(distanceMap);
-	    
-	    List<Integer> indicesPalets = indicesPalets(distanceMap,indicesFiltres);
-	    
-	    ArrayList<ArrayList<Integer>> palets = palets(indicesPalets);
-
-	    Float distanceMin = paletLePlusProche(palets,distanceMap);
-	    
-	    System.out.println("Nombre de mesure: " + distanceMap.size());
-	    System.out.println("Nombre d'indices filtres: " + indicesFiltres.size());
-	    System.out.println("Indices palets: " + indicesPalets);
-	    System.out.println("Nombre de palets visibles: " + palets.size());
-	    System.out.println("Liste de listes des indices des palets: " + palets);
-	    System.out.println("Distance du palet le plus proche: " + distanceMin);
-	}
-
 	
 	public void deposer() {
-		actionneurs.ouvrirPinces(1000);
+		switch (premiereCouleur) {
+		
+		case lejos.robotics.Color.BLUE:
+			deposerVert();
+		
+		case lejos.robotics.Color.GREEN:
+			deposerBlue();
+			
+		default:
+			break;
+		}
 	}
+	
+	public void deposerVert() {
+		
+		actionneurs.setLinearSpeed(100);
+		actionneurs.avancer(Double.MAX_VALUE, true);
+		
+		while(actionneurs.isMoving()) {
+			Delay.msDelay(20);
+			float distance = distanceDevant();
+			if(!(sensors.getColor() == lejos.robotics.Color.WHITE) || distance > 0.3) {
+				continue;
+			}
+			else {
+				actionneurs.arreter();
+				actionneurs.ouvrirPinces(750, true);
+				actionneurs.reculer(35, true);
+				actionneurs.fermerPinces(1000,true);
+				actionneurs.arreter();
+			}
+		}
+		if(orientation < 180) {
+			actionneurs.tourner(90, false);
+		}
+			
+		else {
+			actionneurs.tourner(-90, false);
+		}
+		actionneurs.reculer(80, true);
+		while(actionneurs.isMoving()) {
+			if(!(sensors.getColor() == lejos.robotics.Color.BLACK)) {
+				continue;
+			}
+			else actionneurs.arreter();
+		}
+		if(orientation < 180) {
+			actionneurs.tourner(-90, false);
+		}
+			
+		else {
+			actionneurs.tourner(90, false);
+		}	
+	}
+
+	public void deposerBlue() {
+		
+		actionneurs.setLinearSpeed(100);
+		actionneurs.avancer(Double.MAX_VALUE, true);
+		
+		while(actionneurs.isMoving()) {
+			Delay.msDelay(20);
+			float distance = distanceDevant();
+			if(!(sensors.getColor() == lejos.robotics.Color.WHITE) || distance > 0.3) {
+				continue;
+			}
+			else {
+				actionneurs.arreter();
+				actionneurs.ouvrirPinces(750, true);
+				actionneurs.reculer(35, true);
+				actionneurs.fermerPinces(1000,true);
+				actionneurs.arreter();
+			}
+		}
+		if(orientation < 180) {
+			actionneurs.tourner(-90, false);
+		}
+			
+		else {
+			actionneurs.tourner(90, false);
+		}
+		actionneurs.reculer(80, true);
+		while(actionneurs.isMoving()) {
+			if(!(sensors.getColor() == lejos.robotics.Color.BLACK)) {
+				continue;
+			}
+			else actionneurs.arreter();
+		}
+		if(orientation < 180) {
+			actionneurs.tourner(90, false);
+		}
+			
+		else {
+			actionneurs.tourner(-90, false);
+		}	
+	}
+
+	public void manipulerPalet() {
+		ramasser();
+		allerZoneDepot();
+		deposer();
+	}
+	
+	public void orientation() {
+    	
+    }
 	
 	public void premierPalet() {
-		actionneurs.avancer(500, true);
-		actionneurs.ouvrirPinces(1000);
-		actionneurs.fermerPinces(1000);
-		actionneurs.arc(1230,750);
-		actionneurs.ouvrirPinces(100);
-
-	}
-	
-	public int getCouleurEnTempsReel() {
-	   
-	    SampleProvider colorProvider = sensors.getColor();
-	    float[] sample = new float[colorProvider.sampleSize()];
-	    
-	    
-	    colorProvider.fetchSample(sample, 0);
-	    
-	   
-	    return (int) sample[0];
-	}
-	
-	public void avancerJusquaBlanc() {
-	    actionneurs.setLinearSpeed(200); 
-	    actionneurs.avancer(Double.MAX_VALUE, true); 
-
-	   
-	    while (getCouleurEnTempsReel() != 6) { 
-	        Delay.msDelay(100); 
-	    }
-
-	   
-	    actionneurs.arreter();
-	    System.out.println("Couleur blanche détectée, arrêt du robot.");
-	    deposer();
-	    actionneurs.reculer(150,true);
-	    actionneurs.fermerPinces(1000);
+		premiereCouleur = sensors.getColor();
+		System.out.println("Noir: " + lejos.robotics.Color.BLACK);
+		System.out.println("Blue: " + lejos.robotics.Color.BLUE);
+		System.out.println("Vert: " + lejos.robotics.Color.GREEN);
+		System.out.println("Blanc: " + lejos.robotics.Color.WHITE);
 	}
 	
 	public static void main (String[] args) {
 		
 		Robot R = new Robot();
-		R.rechercher();
-		R.deplacerVersPalet();
-		//R.avancerJusquaBlanc();
-		
+		R.trouverPalet();
+		R.manipulerPalet();
         
     }
 	
 }
-
-   
-
-
